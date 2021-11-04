f68eddb1717730b8146cd76102fccb46fbbef098
Peter Iskandar <piskandar@vmware.com>
2021-11-04 10:25:29 -0400

    5.3

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -15,6 +15,14 @@ public class BeerControllerIT extends BaseIT {
 
 
     @Test
+    void initCreationForm() throws Exception {
+        mockMvc.perform(get("/beers/new").with(httpBasic("user", "password")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/createBeer"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
+    @Test
     @WithMockUser("spring")
     void findBeers() throws Exception {
         mockMvc.perform(get("/beers/find"))
